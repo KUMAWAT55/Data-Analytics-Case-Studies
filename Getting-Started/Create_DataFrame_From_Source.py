9faf828547600973c06fbf07b86eb6203f338d89
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2020-05-20 03:03:42 +0530

    Creating DataFrame from Source

---
+++ Getting-Started/Create_DataFrame_From_Source.py
@@ -0,0 +1,10 @@
+import pandas as pd
+file_name = "https://raw.githubusercontent.com/rajeevratan84/datascienceforbusiness/master/titanic.csv"
+titanic_df = pd.read_csv(file_name)
+print(titanic_df.head(10)) #head returns top 10 records
+print(titanic_df.tail(10))#tail returns last 10 records
+#How to Save
+titanic_df.to_csv("outputs/titanic.csv")
+#Save as excel
+#titanic_df.to_excel("outputs/titanic.xlsx")
+print(titanic_df.info)
