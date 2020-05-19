32b760e2a48f1cf28d9b98fed0bbe6128cc83340
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2020-05-20 03:05:18 +0530

    Creating DataFrame from Source

--- Getting-Started/Create_DataFrame_From_Source.py
+++ Getting-Started/Create_DataFrame_From_Source.py
@@ -1,4 +1,5 @@
 import pandas as pd
+#this file have dummy datasets for titanic passangers .
 file_name = "https://raw.githubusercontent.com/rajeevratan84/datascienceforbusiness/master/titanic.csv"
 titanic_df = pd.read_csv(file_name)
 print(titanic_df.head(10)) #head returns top 10 records
