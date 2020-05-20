c3a5d93d6ce95d9f15e066b93dc30315a5907073
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2020-05-21 03:50:28 +0530

    Discriptive Functionw DataFrame

---
+++ Getting-Started/Discriptive_Functions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+dictionary={'Name':['Rohit','Rahul','A','B'],'Age':[23,25,26,25]}
+DataFrame=pd.DataFrame(dictionary)
+#Unique
+print(DataFrame['Age'].unique())
+print("######")
+# count
+print(DataFrame.count())
+print("######")
+# Mean
+print(DataFrame.mean(Numeric=True))
+#Max can be
+#Min can be
+#value_count
+
+
